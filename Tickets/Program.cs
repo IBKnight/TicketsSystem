8d72963541f511a7cd1d397d694ef1b4373be1ef
Ваня <@DESKTOP-G7FE8PT>
2021-09-07 14:00:25 +0300

    Добавьте файлы проекта.

---
+++ Tickets/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TicketSystem";
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Это программа для информационной системы покупки билетов в театр");
+
+        }
+    }
+}
